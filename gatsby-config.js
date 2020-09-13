1648a72f91e52372466d5e4d535d0c8685fdc36d
cristian salinas <cristiansalinas@MacBook-Pro.iptv.microsoft.com>
2020-09-13 15:50:50 -0300

    added post list

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,18 @@ module.exports = {
           require(`tailwindcss`)
         ]
       }
-    }
+    },
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: "http://localhost:1337",
+        contentTypes: [
+          // List of the Content Types you want to be able to request from Gatsby.
+          "Contest",
+        ],
+        queryLimit: 1000,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/app/Dashboard.js
+++ src/components/app/Dashboard.js
@@ -1,10 +1,104 @@
 import React from 'react'
+import { StaticQuery, graphql } from "gatsby"
 
-const Dashboard = () => (
-  <>
-    <h1>Dashboard</h1>
-    <p>Aqui van los trabajos</p>
-  </>
-)
+class PostRow extends React.Component {
+  render() {
+    const post = this.props.post;
+
+    return (
+      <tr>
+        <td>{post.node.Title}</td>
+        <td>NO</td>
+        <td>Evaluar</td>
+      </tr>
+    );
+  }
+}
+
+class PostTable extends React.Component {
+  render() {
+    const rows = [];
+
+    this.props.posts.forEach((post) => {
+      rows.push(
+        <PostRow
+          post={post}
+          key={post.strapiID} />
+      );
+    });
+
+    return (
+      <table>
+        <thead>
+        <tr>
+          <th>Título</th>
+          <th>Evaluado</th>
+          <th></th>
+        </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Buscar..." />
+      </form>
+    );
+  }
+}
+
+class FilterablePostTable extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <PostTable posts={this.props.posts} />
+      </div>
+    );
+  }
+}
+
+
+
+const Dashboard = ({ children }) => {
+
+  return (
+    <>
+      <h1>Listado</h1>
+      <p>Listado de trabajos en competencia</p>
+      <StaticQuery
+        query={graphql`
+         {
+           allStrapiContest {
+            edges {
+              node {
+                Category
+                Song {
+                  url
+                }
+                Story
+                Title
+                strapiId
+              }
+            }
+          }
+         }
+      `}
+        render={data => (
+          <div className="uk-section">
+            <div className="uk-container uk-container-large">
+              <FilterablePostTable posts={data.allStrapiContest.edges}/>
+            </div>
+          </div>
+        )}
+      />
+    </>
+  )
+}
 
 export default Dashboard
